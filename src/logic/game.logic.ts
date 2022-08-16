efd5ab5f2f7968bf08961a845d443a5c11537f81
schueda <a.schuedamenezes@gmail.com>
2022-08-16 11:30:57 -0300

    Starts the mess with the scan message.

--- src/logic/game.logic.ts
+++ src/logic/game.logic.ts
@@ -1,11 +1,19 @@
 import { MatchmakingState, MatchmakingUser } from '../types/state/matchmaking.state';
 import { GameDatabase } from '../data/game.db';
 import { GameState, Player } from '../types/state/game.state';
-import { defaultGameRules } from '../types/game_rules';
+import { defaultGameRules, GameRules } from '../types/game_rules';
+import { ConnectionService } from './connection.logic';
+import { logger } from '../logger';
+import { defaultTags } from '../types/tags';
+import { SendableMessage, UserIdMessage } from '../types/message';
+import { SchedulableAction } from '../types/action';
+import e = require('express');
+
+export type ScannedMessage = UserIdMessage & { payload: { scanResult: string } };
 
 export class GameService {
 
-    constructor(private db: GameDatabase) { }
+    constructor(private db: GameDatabase, private connSvc: ConnectionService) { }
 
     state: GameState;
 
@@ -21,7 +29,7 @@ export class GameService {
             ingredients: 0,
             poisons: 0,
 
-            attendedMeeting: false,
+            attendedToMeeting: false,
         });
 
         // Generate an array from 0 to player len
@@ -34,11 +42,108 @@ export class GameService {
         }
 
         this.state = <GameState>{
+            players,
+            tasksDone: 0,
+            totalTasks: gameRules.tasksPerWizard * players.filter(p => p.role === "wizard").length,
 
+            meetingCalled: false,
+            meetingHappening: false
         }
-        // TODO: Create the Game based on the MMState
-        // const game = new GameSta(matchmakingState);
 
-        // TODO: Save the created game in the DB
+        this.db.updateGame(this.state);
+    }
+
+    registerScannedMessage() {
+        this.connSvc.registerMessageReceiver("scanned", (message: ScannedMessage) => {
+            logger.debug(`[GameService.registerScannedMessage] Received scanned message ${JSON.stringify(message)}`);
+
+            var messages: SendableMessage[];
+            var actions: SchedulableAction[];
+
+            var originPlayer = this.state.players.find(p => p.id === message.payload.userId);
+
+            if (originPlayer.isAlive) {
+                if (!this.db.getGame().meetingCalled) {
+                    if (defaultTags.playerTags.includes(message.payload.scanResult)) {
+                        var targetPlayer = this.state.players.find(p => p.id === message.payload.scanResult);
+                        
+                        if (originPlayer.taskBeingDone) {
+                            // TODO: FINISH YOUR TASK FIRST MESSAGE
+                        } else {
+                            if (targetPlayer.isAlive) {
+                                if (originPlayer.role === "robot") {
+                                    if (targetPlayer.role === "wizard") {
+                                        if (originPlayer.poisons > 0) {
+                                            if (targetPlayer.poisonTime === undefined) {
+                                                //TODO: POISON REDUCER
+                                            } else {
+                                                //TODO: ALREADY POISONED MESSAGE
+                                            }
+                                        }
+                                        else {
+                                            //TODO: OUT OF POISON
+                                        }
+                                    } else {
+                                        //TODO: CANT POISON ROBOT MESSAGE
+                                    }
+                                } else {
+                                    const task = originPlayer.currentTasks.find(t => t.scanId === message.payload.scanResult);
+                                    if (task) {
+                                        if (defaultGameRules.taskDeliveryMode === "returnCenter") {
+                                            if (originPlayer.ingredients < defaultGameRules.maxIngredients) {
+                                                //TODO: RETURN CENTER REDUCER
+                                            } else {
+                                                //TODO: UNLOAD BAG MESSAGE
+                                            }
+                                        } else {
+                                            //TODO: AUTO-DELIVER REDUCER
+                                        }
+                                    } else {
+                                        //TODO: SHOULDNT SCAN PLAYER MESSAGE
+                                    }
+                                }
+                            } else {
+                                if (targetPlayer.diedRecently) {
+                                    //TODO: SCANNED BODY REDUCER
+                                } else {
+                                    //TODO: SCANNED GHOST MESSAGE
+                                }
+                            }
+                        }
+                    } else if (defaultTags.taskTags.includes(message.payload.scanResult)) {
+                        const task = originPlayer.taskBeingDone;
+                        if (task) {
+                            if (task.scanId === message.payload.scanResult) {
+                                if (defaultGameRules.taskDeliveryMode === "returnCenter") {
+                                    //TODO: FINISH TASK RETURN CENTER REDUCER
+                                } else {
+                                    //TODO: FINISH TASK AUTO DELIVERY REDUCER
+                                }
+                            } else {
+                                //TODO: FINISH TASK WHERE STARTED MESSAGE
+                            }
+                        } else {
+                            if ()
+                        }
+                    }
+                } else {
+                    
+                }
+            } else {
+                //TODO: YOURE DEAD 
+            }
+
+
+            this.db.updateGame(this.state);
+
+            messages.forEach(m => this.connSvc.emit(m));
+
+            this.processActions(actions);
+        })
+    }
+
+    processActions(actions: SchedulableAction[]) {
+        actions.forEach(a => {
+        })
     }
 }
--- src/types/state/game.state.ts
+++ src/types/state/game.state.ts
@@ -1,3 +1,4 @@
+import { SchedulableAction } from '../action';
 import { SendableMessage } from '../message';
 import { GameTask } from '../task';
 
@@ -10,19 +11,48 @@ export type Player = {
     diedRecently: boolean;
     ingredients: number;
     poisons: number;
+    currentTasks: GameTask[];
     taskBeingDone?: GameTask;
 
-    attendedMeeting: boolean;
+    attendedToMeeting: boolean;
     votedPlayer?: string;
 }
 
-export type GameState = {
+export class GameState {
     players: Player[];
     tasksDone: number;
     totalTasks: number;
 
     meetingCalled: boolean;
     meetingHappening: boolean;
+
+    constructor(players: Player[]) {
+        this.players = players;
+        this.tasksDone = 0;
+        this.totalTasks = 0;
+        this.meetingCalled = false;
+        this.meetingHappening = false;
+    }
+
+    getWizards(): Player[] {
+        return this.players.filter(p => p.role === "wizard");
+    }
+
+    getRobots(): Player[] {
+        return this.players.filter(p => p.role === "robot");
+    }
+
+    getAlivePlayers(): Player[] {
+        return this.players.filter(p => p.isAlive);
+    }
+
+    getOnMeetingPlayers(): Player[] {
+        return this.players.filter(p => p.attendedToMeeting);
+    }
+
+    getPlayerById(id: string): Player {
+        return this.players.find(p => p.id === id);
+    }
 }
 
-export type MatchmakingReducerReturn = [GameState, SendableMessage[]];
+export type GameReducerReturn = [GameState, SendableMessage[], SchedulableAction[]];
