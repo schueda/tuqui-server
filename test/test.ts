366e84b536eec144249b48dd9bb241de96b832c3
Enzo Maruffa <enzomm1999@gmail.com>
2022-08-25 15:02:25 -0300

    Fixes the basic jest execution

--- test/test.ts
+++ test/test.ts
@@ -1,44 +1,75 @@
-const App = require("../src/app");
-const Client = require("socket.io-client");
+// const App = require("../src/app");
+import { io as Client } from "socket.io-client";
+import { Server, Socket } from 'socket.io';
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
-describe("my awesome project", () => {
-    let io, serverSocket, clientSocket;
+import { buildApp } from '../src/di';
+import { logger } from '../src/logger';
+import { App } from "../src/app";
+
+process.env.DEBUG = "*"
+
+describe("tuqui-test", () => {
+    let app: App;
+    let serverSocket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
+    let clientSocket;
 
     beforeAll((done) => {
-        console.log("beforeAll");
+        logger.info(`[TEST] beforeAll started`);
 
-        const app = new App();
+        app = buildApp();
+        app.server.listen(() => {
+            clientSocket = Client(`http://localhost:${app.PORT}`, {
+                transports: ["websocket", "polling"], // use WebSocket first, if available
+                query: {
+                    userId: '1'
+                }
+            });
+            logger.debug(`[TEST] clientSocket created. ${clientSocket.connected}`);
 
-        const httpServer = app.server;
-        io = app.io;
-
-        httpServer.listen(() => {
-            const port = 8100;
-            clientSocket = Client(`http://localhost:${port}`);
-            io.on("connection", (socket) => {
+            app.io.on("connection", (socket) => {
+                logger.debug(`[TEST] io.on connection assigned serverSocket`);
                 serverSocket = socket;
             });
-            clientSocket.on("connect", done);
+
+            clientSocket.on("connect", () => {
+                logger.debug(`[TEST] clientSocket.on connected ${clientSocket.id}`);
+                done();
+            });
+
+            clientSocket.on("connect_error", (error) => {
+                logger.debug(`[TEST] clientSocket.on connect_error ${error}`);
+            });
         });
-        console.log("beforeAll done");
+
+        logger.info(`[TEST] beforeAll finished`);
     });
 
     afterAll(() => {
-        console.log("afterAll");
-        io.close();
-        clientSocket.close();
-        console.log("afterAll done");
+        logger.info(`[TEST] afterAll started`);
+
+        serverSocket?.disconnect();
+        clientSocket?.close();
+        app.io.close();
+
+        logger.info(`[TEST] afterAll finished`);
     });
 
-    test("should work", (done) => {
+    test("hello -> hello world", (done) => {
+        logger.info(`[TEST] hello -> hello world starting`);
+
         clientSocket.on("hello", (arg) => {
             expect(arg).toBe("world");
             done();
         });
         serverSocket.emit("hello", "world");
+
+        logger.info(`[TEST] hello -> hello world finished`);
     });
 
-    test("should work (with ack)", (done) => {
+    test("hi -> hola", (done) => {
+        logger.info(`[TEST] hi -> hola starting`);
+
         serverSocket.on("hi", (cb) => {
             cb("hola");
         });
@@ -46,5 +77,7 @@ describe("my awesome project", () => {
             expect(arg).toBe("hola");
             done();
         });
+
+        logger.info(`[TEST] hi -> hola finished`);
     });
 });
