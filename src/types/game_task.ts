b2cc84be91bc93a413420f63b48c0e6f2e321e0b
schueda <a.schuedamenezes@gmail.com>
2022-10-13 11:32:34 -0300

    Changing minigame payload to optional

--- src/types/game_task.ts
+++ src/types/game_task.ts
@@ -1,7 +1,7 @@
 export type GameTask = {
     uuid: string;
     scanId: string;
-    payload: Record<string, unknown>;
+    payload?: Record<string, unknown>;
     type: "blocked" | "blowTheBugs" | "cleanTheGems" | "outOfLab" | "scanThem" | "spellTheSpell"
     completed: boolean;
 }
